796449c0f3819cfa8889cf8d3a384fba64630a85
rina <yan28@myseneca.ca>
2024-03-17 19:42:37 -0400

    newly updated game.py

--- game.py
+++ game.py
@@ -2,3 +2,20 @@
 ##Student Name: Rina An
 ##Student Number:102664224
 
+import random
+
+options = ["rock", "paper", "scissors"] #list of options for the computer
+
+user_option = input("Enter your option (rock, paper, or scissors): ").lower()
+computerOption = random.choice(options) #This will choose a random option from the list for the computer
+
+print(f"\nYou chose {user_option}, computer chose {computerOption}.\n")
+
+if (user_option == "rock" and computerOption == "scissors") or \
+         (user_option == "paper" and computerOption == "rock") or \
+         (user_option == "scissors" and computerOption == "paper"):
+        print("You won the game!")
+elif user_option == computerOption:
+        print("This game is a tie!")
+else:
+        print("Computer won the game!")
