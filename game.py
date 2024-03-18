b720228fe6a8cad1200e43428997dba997b61cf6
rina <yan28@myseneca.ca>
2024-03-17 20:48:18 -0400

    newly updated game.py

--- game.py
+++ game.py
@@ -4,18 +4,25 @@
 
 import random
 
-options = ["rock", "paper", "scissors"] #list of options for the computer
+while True:
 
-user_option = input("Enter your option (rock, paper, or scissors): ").lower()
-computerOption = random.choice(options) #This will choose a random option from the list for the computer
+    options = ["rock", "paper", "scissors"] #list of options for the computer
 
-print(f"\nYou chose {user_option}, computer chose {computerOption}.\n")
+    user_option = input("Enter your option (rock, paper, or scissors): ").lower()
+    computerOption = random.choice(options) #This will choose a random option from the list for the computer
 
-if (user_option == "rock" and computerOption == "scissors") or \
+    print(f"\nYou chose {user_option}, computer chose {computerOption}.\n")
+
+    if (user_option == "rock" and computerOption == "scissors") or \
          (user_option == "paper" and computerOption == "rock") or \
          (user_option == "scissors" and computerOption == "paper"):
         print("You won the game!")
-elif user_option == computerOption:
+    elif user_option == computerOption:
         print("This game is a tie!")
-else:
+    else:
         print("Computer won the game!")
+
+    play_again = input("Continue the game? (yes/no): ")
+    if play_again != 'yes':
+        print("Exit the game")
+        break
